dc8a5a8f2fed85782bfb075bfb39ed484d3f4285
Afonso Roberto Plantes <afonso.plants@gmail.com>
2025-08-26 12:42:58 -0400

    Correção da classe DOG

--- class_dog.py
+++ class_dog.py
@@ -1,9 +1,14 @@
 class Dog:
 
-    tricks = []             # mistaken use of a class variable
-
     def __init__(self, name):
         self.name = name
+        self.tricks = []    # creates a new empty list for each dog
 
     def add_trick(self, trick):
         self.tricks.append(trick)
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.add_trick('roll over')
+e.add_trick('play dead')
+d.tricks
