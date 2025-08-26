1f4fe448975f427876dd09dd174225dd723008a7
Afonso Roberto Plantes <afonso.plants@gmail.com>
2025-08-26 12:41:47 -0400

    função dog incorreta

---
+++ error_file.py
@@ -0,0 +1,14 @@
+import sys
+
+try:
+    f = open('myfile.txt')
+    s = f.readline()
+    i = int(s.strip())
+except OSError as err:
+    print("OS 2 error:", err)
+except ValueError:
+    print("Could not convert data to an integer.")
+except Exception as err:
+    print(f"Unexpected {err=}, {type(err)=}")
+    raise
+
