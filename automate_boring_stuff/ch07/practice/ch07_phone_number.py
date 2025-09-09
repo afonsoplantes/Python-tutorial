29285ee65944900b515e04652b9a4897ec32607c
Afonso Roberto Plantes <afonso.plants@gmail.com>
2025-09-09 17:20:07 -0400

    Ch07 first practice.

---
+++ automate_boring_stuff/ch07/practice/ch07_phone_number.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+
+"""
+Escope:
+    This code will perform a phone number and email address extraction from a clipbord tranfer area.
+    When you press crt+c on a text. 
+Modules:
+    Regex
+    pyperclip - 
+"""
+
